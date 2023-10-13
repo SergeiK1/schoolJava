230d8af1bfeedf9c329098b3a2e54ac5868c768e
SergeiK1 <sergeikdr1@gmail.com>
2023-10-13 09:52:44 -0400

    comments

--- statMachine2.java
+++ statMachine2.java
@@ -9,17 +9,17 @@ public class statMachine2 {
         String instructions = getValue.nextLine().toUpperCase();
         getValue.close();
 
-        String states = "RyopgOpybrYxbxxGbxrxBxrxyPxbgr"; // red orange yellow green blue purple 
-        String letters = "ABCD";
+        String state = "RyopgOpybrYxbxxGbxrxBxrxyPxbgr"; // Heart of the state machine:  red orange yellow green blue purple 
+        String letters = "ABCD"; // for inter-section value (for endSection)
 
 
         for (int i =0; i < instructions.length(); i++){
 
-            char instructionState = instructions.charAt(i);
-            int section = states.indexOf(currentState);
-            int endSection = (letters.indexOf(instructionState)+1);
-            currentState = (char) ((int) states.charAt(section+endSection)-32); // uppercase conversion 
-            if (currentState=='X'){
+            char instructionState = instructions.charAt(i); // current instruction char
+            int section = state.indexOf(currentState); // section color index (red orange yellow...)
+            int endSection = (letters.indexOf(instructionState)+1); // for intersection value (a value b value c value within the main state string)
+            currentState = (char) ((int) state.charAt(section+endSection)-32); // fancy ascii uppercase conversion for character 
+            if (currentState=='X'){ // if error or doesnt exist as a state
                 System.out.println("-- Non Existent --");
                 break;
             }
