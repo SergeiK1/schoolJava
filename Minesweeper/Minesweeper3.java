1ada7fdc2ebb63cff9099550068778cf09f7b24e
SergeiK1 <sergeikdr1@gmail.com>
2023-11-13 13:56:05 -0500

    asds

--- Minesweeper/Minesweeper3.java
+++ Minesweeper/Minesweeper3.java
@@ -16,10 +16,11 @@ public class Minesweeper3 {
         private int adjacentMines;
         private int x, y;
         private JButton button; 
+        private Game game;
 
 
         // New Cell creation
-        public Cell(int xIn, int yIn, boolean isMineIn){
+        public Cell(int xIn, int yIn, boolean isMineIn, Game gameIn){
             x = xIn;
             y = yIn;
             isMine = isMineIn;
@@ -27,6 +28,7 @@ public class Minesweeper3 {
             isRevealed = false;
             setFlag = false;
             button = new JButton();
+            game = gameIn;
 
             // button visuals
             button.setBackground(new Color(237, 155, 51));
@@ -68,6 +70,11 @@ public class Minesweeper3 {
                         }
                     }
                 }
+
+
+                game.gameOver(false);
+
+
                 // add game over logic
                 return;
 
@@ -170,25 +177,35 @@ public class Minesweeper3 {
             gameGoing = true;
             // Board board = new Board(width, height, mineChance);
             UI ui = new UI();
-            ui.drawBoard(cells, mineChance);
+            ui.drawBoard(cells, mineChance, this);
+        }
+        public void gameOver(boolean win) {
+            gameGoing = false;
+            System.out.println("GAME OVER ");
+            if (win) {
+                // win condition
+            } else {
+                // lose condition
+            }
+
         }
     }
 
     // User Interface class for handling visuals and user input
     public class UI {
-        public void drawBoard(Cell[][] cells, double mineChance) {
+        public void drawBoard(Cell[][] cells, double mineChance, Game game) {
             JFrame window = new JFrame();
             JPanel panel = new JPanel();
             panel.setBackground(Color.black);
             panel.setLayout(new GridLayout(cells.length, cells[0].length));
-            boolean clear = true;
+            boolean clear = true; // necessary for when hitting a mine so it doesnt infa looop
 
             // Create new grid of cells
             for (int x = 0; x < cells.length; x++) {
                 for (int y = 0; y < cells[x].length; y++) {
                     boolean isMine = Math.random() < mineChance;
 
-                    Cell cell = new Cell(x, y, isMine); // creates a new cell
+                    Cell cell = new Cell(x, y, isMine, game); // creates a new cell
 
                     cell.getButton().addActionListener(e -> { // create the button action listener to detect onclick
                         cell.handleCellClick(cells, clear);
