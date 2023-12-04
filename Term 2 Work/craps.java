a508f7baade1836aa344497cde7834f9e8d18ed7
SergeiK1 <sergeikdr1@gmail.com>
2023-12-04 15:22:22 -0500

    crapss

--- Term 2 Work/craps.java
+++ Term 2 Work/craps.java
@@ -1,56 +1,85 @@
 
 public class craps {
 
-    public static void main(String[] args) {
-
-        int rollSum = 0;
-        boolean firstRoll = true;
+    public static void runCraps(int[] statsWon, int[] statsLoss){
+        // int rollSum = 0;
+        int numberRoll = 0;
         int currentRoll = 0;
-        char winLose = 'f';
+        boolean lost = false;
         int point=-1;
 
-        while (winLose == 'f') {
+        while (!lost) {
+            numberRoll++;
             currentRoll = ((int) (Math.random()*6+1)) + ((int) (Math.random()*6+1));
-            System.out.println("Roll: "+currentRoll);
+            // System.out.println("Roll: "+currentRoll);
 
-            if (!firstRoll) { // next rolls 
+
+            if (numberRoll > 1) { // next rolls 
+                if (numberRoll > 20){
+                    if (currentRoll == 7) {
+                        statsLoss[20]++;
+                        lost = true;
+                        break;
+                    }
+                    if (currentRoll == point) {
+                        statsWon[20]++;
+                        lost = true;
+                        break;
+                    } 
+                }
                 if (currentRoll == 7) {
-                    winLose = 'l';
+                    statsLoss[numberRoll]++;
+                    lost = true;
                     break;
                 }
                 if (currentRoll == point) {
-                    winLose = 'w';
+                    statsWon[numberRoll]++;
+                    lost = true;
                     break;
                 }
             }
 
-            if (firstRoll){ // all conditions for first roll
+            if (numberRoll == 1){ // all conditions for first roll
                 if (currentRoll == 7 || currentRoll == 11){
-                    winLose = 't';
+                    statsWon[numberRoll]++;
+                    lost = true;
                     break;
+
                 }
                 if (currentRoll == 2 || currentRoll == 3 || currentRoll == 12) {
-                    winLose = 'l';
+                    statsLoss[numberRoll]++;
+                    lost = true;
                     break;
                 }
 
                 point = currentRoll;
-                firstRoll = false;
             }
 
 
         }
-        if (winLose == 't') {
-            System.out.println("YOU WIN!");
-        } else if (winLose == 'l') {
-            System.out.println("YOU LOSE :(");
-        } else {
-            System.out.println("Broke??");
+
+    }
+
+    public static void main(String[] args) {
+
+        int TRIALS = 10000;
+
+        int[] statsWon = new int[21];
+        int[] statsLoss = new int[21];
+
+        for (int i = 0; i < TRIALS; i++){
+            runCraps(statsWon, statsLoss);
         }
-
+        System.out.println("--- Victories ---");
+        for (int j = 1; j<statsWon.length; j++){
+            System.out.println("["+j+"]:  "+statsWon[j]);
+        }
+        System.out.println("--- Defeats ---");
+        for (int k = 1; k<statsLoss.length; k++){
+            System.out.println("["+k+"]:  "+statsLoss[k]);
+        }
        
-
-
+       
     }
 
 
