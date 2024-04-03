74f6782ad67b9f6cc47bfc87bf430cf39c502dba
SergeiK1 <sergeikdr1@gmail.com>
2024-04-03 11:43:37 -0400

    maze done

--- Term3/Maze/Maze.java
+++ Term3/Maze/Maze.java
@@ -3,7 +3,7 @@ package Maze;
 public class Maze {
     
 
-    static int[][] maze = new int[][] { { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1 }, 
+    static int[][] maze1 = new int[][] { { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1 }, 
                                         { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1 },
                                         { 1, 0, 1, 1, 0, 1, 1, 1, 1, 1 }, 
                                         { 1, 0, 0, 0, 0, 1, 1, 1, 1, 1 }, 
@@ -15,21 +15,72 @@ public class Maze {
                                         { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
 
 
+    static int[][] maze2 = new int[][]      {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                                            {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+                                            {1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                                            {1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                                            {1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1},
+                                            {1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1},
+                                            {1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1},
+                                            {1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                            {1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1},
+                                            {1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1},
+                                            {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1},
+                                            {1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1},
+                                            {1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+                                            {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
+                                            {1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+
+    static int[][] maze = new int[][] {
+                                            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                                            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                            {1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+                                            {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                                            {1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+                                            {1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+                                            {1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                                            {1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1},
+                                            {1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1},
+                                            {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                                            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1},
+                                            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+                                            {1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+                                            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                                            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1},
+                                        };
+                                            
+
+
+
+
+    public static void display(int[][] maze) { 
+        for (int i = 0; i < maze.length; i++) { 
+            for (int j = 0; j < maze[i].length; j++) {
+                System.out.print(maze[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.println("\n\n");
+    }
+
     public static boolean solve(int x, int y) { 
-        System.out.println("solve");
+        // System.out.println("solve");
         boolean value = false; 
 
-        if(x < 0 || x > maze.length || y < 0 || y > maze[0].length ) { // checks out of bounds 
+        if(x < 0 || x >= maze.length || y < 0 || y >= maze[0].length ) { // checks out of bounds 
             value = false;
         }
 
         else if (maze[x][y] == 3) { // exit 
+            // System.out.println("Done\n\n");
             value = true;
         } else if (maze[x][y] == 1) { // wall 
             value = false;
         } else if (maze[x][y] == 4) { // traversed 
             value = false; 
         } else if (maze[x][y] == 0) { // open space 
+            maze[x][y] = 4;
             if(solve(x, y+1)) {
                 value =  true; 
             } else if (solve(x+1, y)){ 
@@ -41,14 +92,17 @@ public class Maze {
             }
         }
 
-        maze[x][y] = 4;
+        
+  
         
 
         return value; // base ?? 
     }
 
     public static void main(String[] args) {
-        solve(0,1);
+        solve(3,11);
+        display(maze);
+
     }
 
 }
