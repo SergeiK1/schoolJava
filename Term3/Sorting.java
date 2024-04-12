3ad9bf3599e675425d392f3be8c22f1a97f82fb8
SergeiK1 <sergeikdr1@gmail.com>
2024-04-12 09:55:44 -0400

    sort 1

--- Term3/Sorting.java
+++ Term3/Sorting.java
@@ -34,8 +34,37 @@ public class Sorting {
     }
 
 
+    // ----------------------------------------------------------------------
+
+
+
+    public static ArrayList<Integer> sort1(ArrayList<Integer> list) {
+        while(!isSorted(list)) {
+            for (int i = 0; i < list.size()-1; i++) { 
+                int current = list.get(i);
+                int next = list.get(i+1);
+                if (current > next) { 
+                    list.set(i, next);
+                    list.set(i+1, current);
+                }
+            }
+        }
+        return list;
+    }
+
+
+
+    // ---------------------------------------------------------------------- 
+
+
     public static void main(String[] args) {
-        display(shuffle(fill(10)));
+
+        ArrayList<Integer> list = fill(30);
+        shuffle(list);
+        display(list);
+        sort1(list);
+        display(list);
+        System.out.println(isSorted(list));
 
     }
 
